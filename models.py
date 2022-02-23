0ba536c23b4a1d309120df01dbad5665869f4a09
ridwanullahgold <belloridwan60@gmail.com>
2022-02-23 14:00:01 +0100

    Adding field description in the Item model

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Item(BaseModel):
-    userText: str
-    databaseText: str
+    userText: str = Field(description="The text entered by the user")
+    databaseText: str = Field(description="The text from the database of lists of fraudulent texts")
     
