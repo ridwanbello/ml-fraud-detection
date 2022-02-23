77f5af5dbff126b99bde271fe2af176b2409f6fa
ridwanullahgold <belloridwan60@gmail.com>
2022-02-23 12:26:22 +0100

    Creating example responses and body for Swagger API

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Import fastapi library
-from fastapi import FastAPI, HTTPException
+from fastapi import Body, FastAPI, HTTPException
 
 
 # Import my modules
@@ -13,15 +13,50 @@ app = FastAPI()
 def root():
     return {"Hello": "World"}
 
-@app.post("/api/v1/predict1")
-def predict_with_stopwords(item: Item):
+predict_examples = {
+    "body":{
+        "value": {
+            "userText": "This is a string that is entered by the user",
+            "databaseText": "This is a string that is coming from the database"
+        } 
+    }
+}
+
+predict_responses1 = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "example": {
+                    "percentage": "37.5"
+                }
+            }
+        }
+    }
+}
+
+predict_responses2 = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "example": {
+                    "percentage": "48.38"
+                }
+            }
+        }
+    }
+}
+
+@app.post("/api/v1/predict1", responses=predict_responses1)
+def predict_with_stopwords(item: Item = Body(..., examples=predict_examples)):
     if len(item.userText.split()) < 4 or len(item.databaseText.split()) < 4 :
         raise HTTPException(status_code=404, detail="Text should not be less than 4 words")
     result = PlagiarismChecker(item.userText,item.databaseText)
     return {"percentage": result.get_rate()}
 
-@app.post("/api/v1/predict2")
-def predict_without_stopwords(item: Item):
+@app.post("/api/v1/predict2", responses=predict_responses2)
+def predict_without_stopwords(item: Item = Body(..., examples=predict_examples)):
     if len(item.userText.split()) < 4 or len(item.databaseText.split()) < 4 :
         raise HTTPException(status_code=404, detail="Text should not be less than 4 words")
     result = PlagiarismWithoutStopWords(item.userText,item.databaseText)
