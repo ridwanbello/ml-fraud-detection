53c2fe3da1326de80e844830e13f33b99cefe160
ridwanullahgold <belloridwan60@gmail.com>
2022-02-23 11:13:54 +0100

    Adding 4 words limit for a text

--- main.py
+++ main.py
@@ -13,20 +13,17 @@ app = FastAPI()
 def root():
     return {"Hello": "World"}
 
-@app.get("/prediction")
-def prediction():
-    checker = PlagiarismChecker("I am a boy going to the market","I am a girl going to the market")
-    return {"percentage": checker.get_rate()}
-
 @app.post("/api/v1/predict1")
-def predict(item: Item):
+def predict_with_stopwords(item: Item):
     if len(item.userText.split()) < 4 or len(item.databaseText.split()) < 4 :
         raise HTTPException(status_code=404, detail="Text should not be less than 4 words")
     result = PlagiarismChecker(item.userText,item.databaseText)
     return {"percentage": result.get_rate()}
 
 @app.post("/api/v1/predict2")
-def predict(item: Item):
+def predict_without_stopwords(item: Item):
+    if len(item.userText.split()) < 4 or len(item.databaseText.split()) < 4 :
+        raise HTTPException(status_code=404, detail="Text should not be less than 4 words")
     result = PlagiarismWithoutStopWords(item.userText,item.databaseText)
     return {"percentage": result.get_rate()}
 
