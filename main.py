c3e9261695adc7ac1fa0b144b49bdad093fa50dc
ridwanullahgold <belloridwan60@gmail.com>
2022-03-22 14:09:00 +0100

    Database schema and remote origin added

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # Import fastapi library
 from dataclasses import replace
+from typing import List
 from unittest import result
 from fastapi import Body, FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
-#from sqlalchemy.orm import Session
-#from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal
 from urllib.request import urlopen
 import time, requests
 from numpy import argmax
 # Import my modules
-from schemas import Item, Attribute
+from schemas import Items, Attribute, User
+import crud, models, schemas
 #import models
+
+from sqlalchemy.orm.session import Session
 from PlagiarismChecker import PlagiarismChecker
 from PlagiarismWithoutStopWords import PlagiarismWithoutStopWords
 
@@ -20,10 +24,20 @@ app = FastAPI(
     version="0.1.0",
     debug=True)
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 # CORS Origin Middleware
 origins = [
     "http://localhost:5000",
     "http://localhost:3000",
+    "https://sojiare.com",
+    "https://sojiare.org",
 ]
 
 app.add_middleware(
@@ -33,15 +47,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-#models.Base.metadata.create_all(bind=engine)
-# Dependency
-# def get_database_session():
-#     db = None
-#     try:
-#         db = SessionLocal()
-#         yield db
-#     finally:
-#         db.close()
+
 
 # Routes
 @app.get("/")
@@ -170,14 +176,12 @@ def clean_data(e):
         raise HTTPException(status_code=400, detail=validation)
     return convertedData
 
-
-
 @app.post("/api/v1/predict1", responses=text_responses)
-def predict_with_stopwords(item: Item = Body(..., examples=text_examples)):
+def predict_with_stopwords(item: Items = Body(..., examples=text_examples)):
     if len(item.userText.split()) < 4 :
         raise HTTPException(status_code=400, detail="Text should not be less than 4 words")
     try:    
-        databaseText = "http://sojiare.com/privatedocs/Email_scams.txt"
+        databaseText = "https://justicelab.io/privatedocs/Email_scams.txt"
         data = get_url_content_only(databaseText)
         data = data.split('\r\n')
     except:
@@ -194,11 +198,11 @@ def predict_with_stopwords(item: Item = Body(..., examples=text_examples)):
     return {"percentage": max_value, "text": data[max_index], "frequency": frequency}
 
 @app.post("/api/v1/predict2", responses=text_responses)
-def predict_without_stopwords(item: Item = Body(..., examples=text_examples)):
+def predict_without_stopwords(item: Items = Body(..., examples=text_examples)):
     if len(item.userText.split()) < 4 :
         raise HTTPException(status_code=400, detail="Text should not be less than 4 words")
     try:
-        databaseText = "http://sojiare.com/privatedocs/Email_scams.txt"
+        databaseText = "https://justicelab.io/privatedocs/Email_scams.txt"
         data = get_url_content_only(databaseText)
         data = data.split('\r\n')
     except:
@@ -216,7 +220,7 @@ def predict_without_stopwords(item: Item = Body(..., examples=text_examples)):
 @app.post("/api/v1/check_attribute", responses=check_responses)
 def check_attribute(attribute: Attribute = Body(..., examples=check_examples)):
     try:
-        attributeTxt = get_url_content_only("http://sojiare.com/privatedocs/scamPhones.txt")
+        attributeTxt = get_url_content_only("https://justicelab.io/privatedocs/scamPhones.txt")
         attributeList = attributeTxt.split('\r\n')
         attributeSet = set(attributeList)
         results = []
@@ -249,8 +253,8 @@ def check_attribute(attribute: Attribute = Body(..., examples=check_examples)):
 @app.post("/api/v1/check_attribute_with_text", responses=check_responses)
 def check_attribute_with_text(attribute: Attribute = Body(..., examples=check_examples)):
     try:
-        attributeTxt = get_url_content_only("http://sojiare.com/privatedocs/scamPhones.txt")
-        wordTxt = get_url_content_only("http://sojiare.com/privatedocs/Email_scams.txt")
+        attributeTxt = get_url_content_only("https://justicelab.io/privatedocs/scamPhones.txt")
+        wordTxt = get_url_content_only("https://justicelab.io/privatedocs/Email_scams.txt")
         attributeList = attributeTxt.split('\r\n')
         attributeSet = set(attributeList)
         wordList = wordTxt.split('\r\n')
@@ -289,3 +293,37 @@ def check_attribute_with_text(attribute: Attribute = Body(..., examples=check_ex
                     results.append(cleanedData)
     return {"matched": results}
 
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    
+    for i, data in enumerate(users):
+        yield data.email
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
--- schemas.py
+++ schemas.py
@@ -1,10 +1,38 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
-
-class Item(BaseModel):
+class Items(BaseModel):
     userText: str = Field(description="The text entered by the user")
     # databaseText: str = Field(description="The text from the database of lists of fraudulent texts")
 
 class Attribute(BaseModel):
     features: str = Field(description="The features entered by the user. Can be phone number, link or email, separated by comma if more than one")
-    
+  
+class ItemBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+class ItemCreate(ItemBase):
+    pass
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+class UserBase(BaseModel):
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: list[Item] = []
+
+    class Config:
+        orm_mode = True
+
